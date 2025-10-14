8a4647f86f82c9edb9d4861c2d99f0897c4af6ff
AloneRunner <kaanozarik@gmail.com>
2025-10-14 17:03:33 +0300

    Restrict CORS origins to Netlify and localhost

--- server/main.py
+++ server/main.py
@@ -96,7 +96,7 @@ class SolveRequest(BaseModel):
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://ozariktable.netlify.app", "http://localhost:5173", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
